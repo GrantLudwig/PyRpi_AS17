3d0742596907fcdceb841835a822e8d9ee8101ca
GrantLudwig <grantludwig97@gmail.com>
2019-11-26 19:23:11 -0800

    initial commit

---
+++ ludwig_grant_AS17.py
@@ -0,0 +1,8 @@
+# File Name: ludwig_grant_AS17.py
+# File Path: /home/ludwigg/Python/PyRpi_AS17/ludwig_grant_AS17.py
+# Run Command: sudo python3 /home/ludwigg/Python/PyRpi_AS17/ludwig_grant_AS17.py
+
+# Grant Ludwig
+# 12/6/2019
+# AS.17
+# Create a custom class to display an animated graphics object
